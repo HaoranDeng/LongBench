36dc6122124681c7cb27051bd24351302c0b7be1
HaoranDeng <deng.hr@163.com>
2025-09-10 16:52:25 +0000

    update

---
+++ LongBench/scripts/run_book_odls.sh
@@ -0,0 +1,9 @@
+MODEL_PATH="book-odls-1000"
+# MODEL_PATH="llama2-7b-chat-4k"
+
+python pred.py --model $MODEL_PATH
+
+python eval.py --model $MODEL_PATH
+
+BLOB_PATH="/mnt/blob-pretraining-hptraining/haoran_results/LongBench"
+mv pred/${model_name} $BLOB_PATH/${model_name}
