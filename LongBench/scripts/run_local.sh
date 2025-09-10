043794776c2578e1f4ad3103f6a079bfb666c339
HaoranDeng <deng.hr@163.com>
2025-09-10 12:18:10 +0000

    update

--- LongBench/scripts/run_local.sh
+++ LongBench/scripts/run_local.sh
@@ -1,3 +1,3 @@
 MODEL_PATH="/mnt/blob-pretraining-hptraining/long_corpus/checkpoints/lcft_Meta-Llama-3-8B_ready_code-odl/checkpoint-1000"
-MODEL_PATH="chatglm2-6b"
+MODEL_PATH="llama2-7b-chat-4k"
 python pred.py --model $MODEL_PATH
