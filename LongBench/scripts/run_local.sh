901936c0721434ed4d16e9022e272eb30180f5cd
HaoranDeng <deng.hr@163.com>
2025-09-11 07:19:43 +0000

    update

--- LongBench/scripts/run_local.sh
+++ LongBench/scripts/run_local.sh
@@ -3,4 +3,5 @@ python pred.py --model $model_name
 python eval.py --model $model_name
 BLOB_PATH="/mnt/blob-pretraining-hptraining/haoran_results/LongBench"
 mkdir -p $BLOB_PATH
+ls -al pred/
 mv pred/${model_name} ${BLOB_PATH}/${model_name}
