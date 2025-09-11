e7d2b09ca902192ab6a8502f6374381cff444e92
HaoranDeng <deng.hr@163.com>
2025-09-11 09:15:38 +0000

    update

--- LongBench/scripts/run_book_odls.sh
+++ LongBench/scripts/run_book_odls.sh
@@ -1,6 +1,7 @@
 model_name="book-odls-1000"
 python pred.py --model $model_name
 python eval.py --model $model_name
-BLOB_PATH="/mnt/blob-pretraining-hptraining/haoran_results/LongBench"
+
+BLOB_PATH="/mnt/blob-pretraining-hptraining/haoran_result/LongBench"
 mkdir -p $BLOB_PATH
 mv pred/${model_name} ${BLOB_PATH}/${model_name}
--- LongBench/scripts/run_local.sh
+++ LongBench/scripts/run_local.sh
@@ -1,7 +1,7 @@
 model_name="llama2-7b-chat-4k"
 python pred.py --model $model_name
 python eval.py --model $model_name
-BLOB_PATH="/mnt/blob-pretraining-hptraining/haoran_results/LongBench"
+BLOB_PATH="/mnt/blob-pretraining-hptraining/haoran_result/LongBench"
 mkdir -p $BLOB_PATH
 ls -al pred/
 mv pred/${model_name} ${BLOB_PATH}/${model_name}
