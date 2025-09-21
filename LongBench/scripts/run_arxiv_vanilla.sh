7f26e281ccd0f08b383c9250ec771237e1c7a51a
HaoranDeng <deng.hr@163.com>
2025-09-21 09:29:42 +0000

    update

---
+++ LongBench/scripts/run_arxiv_vanilla.sh
@@ -0,0 +1,7 @@
+model_name="arxiv-vanilla"
+python pred.py --model $model_name
+python eval.py --model $model_name
+
+BLOB_PATH="/mnt/blob-pretraining-hptraining/haoran_result/LongBench"
+mkdir -p $BLOB_PATH
+mv pred/${model_name} ${BLOB_PATH}/${model_name}
---
+++ LongBench/scripts/run_arxiv_wj55.sh
@@ -0,0 +1,7 @@
+model_name="arxiv-wj55"
+python pred.py --model $model_name
+python eval.py --model $model_name
+
+BLOB_PATH="/mnt/blob-pretraining-hptraining/haoran_result/LongBench"
+mkdir -p $BLOB_PATH
+mv pred/${model_name} ${BLOB_PATH}/${model_name}
